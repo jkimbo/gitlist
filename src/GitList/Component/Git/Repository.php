dd026c12d05a2af03c3b60a7e690700b85da28a5
Jonathan Kim <jkimbo@gmail.com>
2012-08-15 22:24:43 +0100

    Removed subject from commit body

--- src/GitList/Component/Git/Repository.php
+++ src/GitList/Component/Git/Repository.php
@@ -329,16 +329,23 @@ class Repository
             throw new \RuntimeException('No commit log available');
         }
 
-				$pos = strpos($logs, '}');
-				$message = substr($logs, 0, $pos+1);
-				$extra = substr($logs, $pos+1);
-				$message = str_replace("\n", ' - ', $message);
-				$extras = explode("\n", $extra);
+        $pos = strpos($logs, '}');
+        $message = substr($logs, 0, $pos+1);
+        $extra = substr($logs, $pos+1);
+        $message = str_replace("\n", ' - ', $message);
+        $extras = explode("\n", $extra);
 
         // Read commit metadata
         $data = json_decode($message, true);
-				$data['message'] = str_replace('-', ' ', $data['message']);
-				$data['body'] = str_replace(' - ', "\n", $data['body']);
+        $data['message'] = str_replace('-', ' ', $data['message']);
+        $data['body'] = str_replace(' - ', "\n", $data['body']);
+
+        // remove subject line
+        $tmpbody = explode("\n", $data['body']);
+        unset($tmpbody[0]);
+        $tmpbody = implode("\n", $tmpbody);
+        $data['body'] = trim($tmpbody);
+
         $commit = new Commit;
         $commit->importData($data);
         //unset($logs[0]);
@@ -419,35 +426,35 @@ class Repository
         $commit->setDiffs($diffs);
 
         return $commit;
-  	}
+    }
 
-		/**
-		 * Get most recent commit
-		 */
-		public function getRecentCommit($branch) 
-		{
-			$head = file_get_contents($this->getPath().'/.git/refs/heads/'.$branch);
+    /**
+     * Get most recent commit
+     */
+    public function getRecentCommit($branch) 
+    {
+      $head = file_get_contents($this->getPath().'/.git/refs/heads/'.$branch);
       //$commit = $this->getCommit($head);
-			$logs = $this->getClient()->run($this, 'show --pretty=format:\'{"hash": "%H", "short_hash": "%h", "tree": "%T", "parent": "%P", "author": "%an", "author_email": "%ae", "date": "%at", "commiter": "%cn", "commiter_email": "%ce", "commiter_date": "%ct", "message": "%f", "body": "%B"}\' ' . $commitHash);
+      $logs = $this->getClient()->run($this, 'show --pretty=format:\'{"hash": "%H", "short_hash": "%h", "tree": "%T", "parent": "%P", "author": "%an", "author_email": "%ae", "date": "%at", "commiter": "%cn", "commiter_email": "%ce", "commiter_date": "%ct", "message": "%f", "body": "%B"}\' ' . $commitHash);
 
-			if (empty($logs)) {
-					throw new \RuntimeException('No commit log available');
-			}
+      if (empty($logs)) {
+          throw new \RuntimeException('No commit log available');
+      }
 
-			$pos = strpos($logs, '}');
-			$message = substr($logs, 0, $pos+1);
-			$extra = substr($logs, $pos+1);
-			$message = str_replace("\n", ' - ', $message);
-			$extras = explode("\n", $extra);
+      $pos = strpos($logs, '}');
+      $message = substr($logs, 0, $pos+1);
+      $extra = substr($logs, $pos+1);
+      $message = str_replace("\n", ' - ', $message);
+      $extras = explode("\n", $extra);
 
-			// Read commit metadata
-			$data = json_decode($message, true);
-			$data['message'] = str_replace('-', ' ', $data['message']);
-			$data['body'] = str_replace(' - ', "\n", $data['body']);
-			$commit = new Commit;
-			$commit->importData($data);
-			return $commit;
-		}
+      // Read commit metadata
+      $data = json_decode($message, true);
+      $data['message'] = str_replace('-', ' ', $data['message']);
+      $data['body'] = str_replace(' - ', "\n", $data['body']);
+      $commit = new Commit;
+      $commit->importData($data);
+      return $commit;
+    }
 
     public function getAuthorStatistics()
     {
