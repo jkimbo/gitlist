6f0c5dd5736eaa867b71a427edf5470edbf29a17
Jonathan Kim <jkimbo@gmail.com>
2012-08-19 23:01:23 +0100

    Added some grammar corrections to human date and added links to commits in commit list

--- src/GitList/Util/View.php
+++ src/GitList/Util/View.php
@@ -46,68 +46,68 @@ class View
         );
     }
 
-		/**
-		 * This function prints the difference between two php datetime objects
-		 * in a more human readable form
-		 * inputs should be like strtotime($date)
-		 * Adapted from https://gist.github.com/207624 python version
-		 * From https://gist.github.com/1053741
-		 */
-		public static function getDate($date, $otherDate=null, $offset=null){
-			if($otherDate == null) {
-				$otherDate = mktime();
-			}
+    /**
+     * This function prints the difference between two php datetime objects
+     * in a more human readable form
+     * inputs should be like strtotime($date)
+     * Adapted from https://gist.github.com/207624 python version
+     * From https://gist.github.com/1053741
+     */
+    public static function getDate($date, $otherDate=null, $offset=null){
+      if($otherDate == null) {
+        $otherDate = mktime();
+      }
 
-			if(!is_int($date)) {
-				$date = strtotime($date);
-			}
+      if(!is_int($date)) {
+        $date = strtotime($date);
+      }
 
-			$offset = $otherDate - $date;
+      $offset = $otherDate - $date;
 
-			if($offset != null){
-				$deltaS = $offset%60;
-				$offset /= 60;
-				$deltaM = $offset%60;
-				$offset /= 60;
-				$deltaH = $offset%24;
-				$offset /= 24;
-				$deltaD = ($offset > 1)?ceil($offset):$offset;
-			} else {
-				throw new \Exception("Must supply otherdate or offset (from now)");
-			}
-			if($deltaD > 1){
-				if($deltaD > 365){
-					$years = ceil($deltaD/365);
-					if($years ==1){
-						return "last year"; 
-					} else{
-						return "$years years ago";
-					}	
-				}
-				/*
-				if($deltaD > 6){
-					return date('d-M',strtotime("$deltaD days ago"));
-				}		
-				 */
-				return "$deltaD days ago";
-			}
-			if($deltaD == 1){
-				return "Yesterday";
-			}
-			if($deltaH == 1){
-				return "last hour";
-			}
-			if($deltaM == 1){
-				return "last minute";
-			}
-			if($deltaH > 0){
-				return $deltaH." hours ago";
-			}
-			if($deltaM > 0){
-				return $deltaM." minutes ago";
-			}
-			else{
-				return "few seconds ago";
-			}
-		}
+      if($offset != null){
+        $deltaS = $offset%60;
+        $offset /= 60;
+        $deltaM = $offset%60;
+        $offset /= 60;
+        $deltaH = $offset%24;
+        $offset /= 24;
+        $deltaD = ($offset > 1)?ceil($offset):$offset;
+      } else {
+        throw new \Exception("Must supply otherdate or offset (from now)");
+      }
+      if($deltaD > 1){
+        if($deltaD > 365){
+          $years = ceil($deltaD/365);
+          if($years ==1){
+            return "last year";
+          } else{
+            return "$years years ago";
+          }
+        }
+        /*
+        if($deltaD > 6){
+          return date('d-M',strtotime("$deltaD days ago"));
+        }
+         */
+        return "$deltaD days ago";
+      }
+      if($deltaD == 1){
+        return "yesterday";
+      }
+      if($deltaH == 1){
+        return "in the last hour";
+      }
+      if($deltaM == 1){
+        return "in the last minute";
+      }
+      if($deltaH > 0){
+        return $deltaH." hours ago";
+      }
+      if($deltaM > 0){
+        return $deltaM." minutes ago";
+      }
+      else{
+        return "a few seconds ago";
+      }
+    }
 }
